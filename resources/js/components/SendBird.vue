f37ef8cca7915645df39871b2bf8703911e84bfe
kim jakyoung <jakyoung.kim@catenoid.net>
2018-09-19 17:05:13 +0900

    :

---
+++ resources/js/components/SendBird.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SendBird",
+        data () {
+            return {
+                msg: "SENDBIRD TEST"
+            }
+        },
+        created: function () {
+            alert("test")
+        }
+    }
+</script>
+
+<style scoped>
+</style>
