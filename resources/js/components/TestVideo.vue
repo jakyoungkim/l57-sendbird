74f8bf6228a086659723a42e747b95137e5f81ab
kim jakyoung <jakyoung.kim@catenoid.net>
2018-10-04 17:26:34 +0900

    코드정리

---
+++ resources/js/components/TestVideo.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TestVideo"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/spinner.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "spinner"
+    }
+</script>
+
+<style scoped>
+
+</style>
